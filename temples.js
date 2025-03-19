f371f7d27e49a8fd0995992b0f2cf19a90601ac2
[JJL04] <lukejayden42@gmail.com>
2025-03-19 12:35:09 -0600

    moved

--- temples.js
+++
@@ -1,9 +0,0 @@
-// Set the copyright year and last modified date dynamically
-document.getElementById('year').textContent = new Date().getFullYear();
-document.getElementById('lastModified').textContent = document.lastModified;
-
-// Hamburger menu functionality
-document.querySelector('.hamburger').addEventListener('click', function () {
-    const navMenu = document.querySelector('nav ul');
-    navMenu.classList.toggle('open');
-});
